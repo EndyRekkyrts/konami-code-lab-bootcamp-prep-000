2e9c0eefb321eef7947d74d6b4d586d437e5f0a2
Carrollyn Catanzaro <endyrekkyrts@yahoo.com>
2018-02-26 18:47:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
-  onEnter(e) {
+  document.addEventListener('keydown',e) {
     const key = parseInt(e.detail);
     if (key === code[index]) {
       index++;
