e567aa112b97a5acd1f32914231f7c22e10bb5e9
Carrollyn Catanzaro <endyrekkyrts@yahoo.com>
2018-02-26 18:44:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0;
+
 function init() {
+  onKeyDownHandler(e) {
+    const key = parseInt(e.detail);
+    if (key === code[index]) {
+      index++;
+    if (index === code) {
+      alert("WOOOHOOOO! You've got the code!")
+      index = 0;
+    }
+    }
+    else {
+      index = 0;
+    }
+  }
   // Write your JavaScript code inside the init() function
 
 }
